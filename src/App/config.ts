77a5ed3f94046d629ecbcfcbc16392597b34ec57
Edison Pacheco <80083216+G-dark@users.noreply.github.com>
2025-05-24 22:04:04 -0500

    Update config.ts

--- src/App/config.ts
+++ src/App/config.ts
@@ -5,6 +5,7 @@ const { Pool } = pkg;
 config();
 
 export const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 const DB_PORT = process.env.DB_PORT;
@@ -13,7 +14,7 @@ const PASSWORD = process.env.PASSWORD;
 export const JWT_SECRET_KEY= process.env.JWT_SECRET_KEY;
 export const PEPPER= process.env.PEPPER || "loquesea";
 export const SALT_OR_ROUNDS= Number(process.env.SALT_OR_ROUNDS) || 10;
-const base = (DB_HOST == "localhost") ? ("http://") : ("https://");
+const base = (HOST == "localhost") ? ("http://") : ("https://");
 export const URL_BASE = base + DB_HOST + ":" + PORT + "/"
  const poolConfig:any = {
   host: DB_HOST,
